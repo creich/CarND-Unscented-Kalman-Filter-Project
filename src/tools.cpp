c6eae6d2e425889127033a7da62fefe6b7db19bd
creich <creich@linux.com>
2019-01-04 20:44:48 +0100

    implemented Tools::CalculateRMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,4 +1,5 @@
 #include "tools.h"
+#include <iostream>
 
 using Eigen::VectorXd;
 using std::vector;
@@ -12,4 +13,38 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  if( (estimations.size() == 0) || (ground_truth.size() == 0) ) {
+     std::cerr << "ERROR: empty estimations OR ground_truth vector! .. quitting" << std::endl;
+     return rmse;
+  }
+
+  //  * the estimation vector size should equal ground truth vector size
+  if( estimations.size() != ground_truth.size() ) {
+     std::cerr << "ERROR: estimations vector and ground_truth vector have different sizes! .. quitting" << std::endl;
+     return rmse;
+  }
+
+  // accumulate squared residuals
+  VectorXd squared_res(4);
+  squared_res << 0, 0, 0, 0;
+
+  for (int i=0; i < estimations.size(); ++i) {
+     VectorXd c = estimations[i] - ground_truth[i];
+     VectorXd s_res = c.array() * c.array();
+     squared_res = squared_res + s_res;
+  }
+
+  // calculate the mean
+  VectorXd mean = squared_res / estimations.size();
+
+  // calculate the squared root
+  rmse = mean.array().sqrt();
+
+  // return the result
+  return rmse;
 }
